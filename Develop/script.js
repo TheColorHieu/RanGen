8d0839224502cdc6b3ae1140dbced3f4c66a1ae1
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-10 19:41:21 -0700

    added the coditions

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,25 @@ function writePassword() {
   passwordText.value = password;
 
 }
+generateBtn.addEventListener("click", writePassword);
 
+//creating the function to generate the password 
+function generatePassword() {
+//developing the string password
+var password = "";
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+const passwordLength = prompt("How long would you like your password?");
+//series of prompt or confirmed. having a prompts allow user to say yes while confirmed sends a true or false statement
+//added codes 
+var lowercase = confirm("Do you want lowercase?");
+console.log(lowercase)
+var upercase = confirm("Do you want uppercase?");
+var numeric = confirm("Do you want to add an umber?");
+var special = confirm("Do you want a special character?");
+
+return password;
+}
+
