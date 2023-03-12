d9acc1990dbf7cea7c977cb7d79f8135296a02e6
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-11 23:56:39 -0700

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//entering characters variables 
+var numbers = "0123456789";
+var uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase ="abcdefghijklmnopqrstuvwxyz"
+var special = "!@#$%^&*()";
+
 
 // Write password to the #password input
 function writePassword() {
@@ -21,12 +27,15 @@ generateBtn.addEventListener("click", writePassword);
 
 const passwordLength = prompt("How long would you like your password?");
 //series of prompt or confirmed. having a prompts allow user to say yes while confirmed sends a true or false statement
-//added codes 
+//next couple lines shows the user option for their passwords
 var lowercase = confirm("Do you want lowercase?");
-console.log(lowercase)
+
 var upercase = confirm("Do you want uppercase?");
-var numeric = confirm("Do you want to add an umber?");
+
+var numeric = confirm("Do you want to add an number?");
+
 var special = confirm("Do you want a special character?");
+
 
 return password;
 }
