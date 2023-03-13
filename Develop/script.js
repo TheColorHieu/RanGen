53bbbb76d639bb6c6d1da329dfefb0d0194e0a49
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-12 21:53:13 -0700

    added if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //entering characters variables 
-var numbers = "0123456789";
-var uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowercase ="abcdefghijklmnopqrstuvwxyz"
-var special = "!@#$%^&*()";
+var numbersOption = '0123456789';
+var uppercaseOption ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowercaseOption ='abcdefghijklmnopqrstuvwxyz';
+var specialOption = '!@#$%^&*()';
+
 
 
 // Write password to the #password input
@@ -13,7 +14,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 generateBtn.addEventListener("click", writePassword);
 
@@ -24,19 +24,51 @@ var password = "";
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+//length of password 
 const passwordLength = prompt("How long would you like your password?");
+//next line of code helps us return the length of the password for future code 
+
+
 //series of prompt or confirmed. having a prompts allow user to say yes while confirmed sends a true or false statement
 //next couple lines shows the user option for their passwords
-var lowercase = confirm("Do you want lowercase?");
+//start of if statements
 
-var upercase = confirm("Do you want uppercase?");
-
-var numeric = confirm("Do you want to add an number?");
-
-var special = confirm("Do you want a special character?");
-
-
-return password;
+var lowerCase = confirm("Do you want lowercase?");
+if(lowerCase === true){
+password = password +getRandomValue(lowerCase);  
 }
 
+var upperCase = confirm("Do you want uppercase?");
+if(upperCase === true){
+  password = password +getRandomValue(upperCase); 
+}
+
+var numbers = confirm("Do you want to add an number?");
+if(numbers === true){
+  password = password +getRandomValue(numbers); 
+}
+
+var special = confirm("Do you want a special character?");
+if(special === true){
+  password = password +getRandomValue(special);
+}
+
+
+
+
+//for loop 
+for (let i =password.length; i < length; i++){
+  password = password +getRandomValue(numbers);
+  password = password +getRandomValue(upperCase);
+  password = password +getRandomValue(lowerCase);
+  password = password +getRandomValue(special);
+
+}
+
+return password.substring(0,passwordLength);
+}
+
+function getRandomValue(str){
+  const randomIndex = Math.floor(Math.random() * str.length);
+  returnstr[randomIndex];
+}
