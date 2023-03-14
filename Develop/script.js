655a25be69694d3330fd944fb3e95a1732657018
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-13 20:32:30 -0700

    generating a password is functional but not random...

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 //entering characters variables 
 var numbersOption = " 0123456789";
-var uppercaseOption ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercaseOption ="abcdefghijklmnopqrstuvwxyz";
+var upperCaseOption ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseOption ="abcdefghijklmnopqrstuvwxyz";
 var specialOption = "!@#$%^&*()";
 
 // Add event listener to generate button
@@ -27,11 +27,18 @@ var password = "";
 
 //length of password 
 var passwordLength = prompt("How long would you like your password?");
+
+//next couple lines shows the user option for their passwords
 //series of prompt or confirmed. having a prompts allow user to say yes while confirmed sends a true or false statement
   var lowerCase = confirm("Do you want a Lowercase?")
   var upperCase = confirm("Do you want a Upperrcase?")
   var numbers = confirm("Do you want a Number?")
   var specialChar = confirm("Do you want a Special Character?")
+  console.log(passwordLength);
+  console.log(lowerCase);
+  console.log(upperCase);
+  console.log(numbers);
+  console.log(specialChar);
 //next line helps user know that they need at to say yes to at least one of the conditions
 if ( passwordLength <= 8 || passwordLength >= 128){
   alert("Your password is not long enough. Please try again.");
@@ -42,51 +49,33 @@ else if ( upperCase === false && numbers === false && specialChar === false){
   var upperCase = confirm("Do you want a Upperrcase?")
   var numbers = confirm("Do you want a Number?")
   var specialChar = confirm("Do you want a Special Character?")
-  
+
+
 }
-
-
-
-//next couple lines shows the user option for their passwords
 //start of if statements
-
-// var lowerCase = confirm("Do you want lowercase?");
-// if(lowerCase === true){
-// password = password +getRandomValue(lowerCase);  
-// }
-
-// var upperCase = confirm("Do you want uppercase?");
-// if(upperCase === true){
-//   password = password +getRandomValue(upperCase); 
-// }
-
-// var numbers = confirm("Do you want to add an number?");
-// if(numbers === true){
-//   password = password +getRandomValue(numbers); 
-// }
-
-// var specialChar = confirm("Do you want a special character?");
-// if(special === true){
-//   password = password +getRandomValue(special);
-// }
+if (lowerCase){
+  password += lowerCaseOption;
+}
+if (upperCase){
+  password += upperCaseOption;
+}
+if (numbers){
+  password += numbersOption;
+}
+if (specialChar){
+  password += specialChar;
+}
 
 
 
 
-//for loop 
+//for loop to help create the password
 for (let i =password.length; i < length; i++){
-  password = password +getRandomValue(numbers);
-  password = password +getRandomValue(upperCase);
-  password = password +getRandomValue(lowerCase);
-  password = password +getRandomValue(special);
-
+  password += password.charAt(Math.floor(Math.random() * passwordLength))
 }
 
-return password.substring(0,passwordLength);
+return password
 }
 
-function getRandomValue(str){
-  const randomIndex = Math.floor(Math.random() * str.length);
-  returnstr[randomIndex];
-}
+
 }
