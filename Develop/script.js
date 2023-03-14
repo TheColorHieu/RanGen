2ae57c9945368164263f837f75f87c1e09a4acaf
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-03-13 20:14:38 -0700

    added alert to let the user know about choosing one of the conditions

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,8 @@ var uppercaseOption ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercaseOption ="abcdefghijklmnopqrstuvwxyz";
 var specialOption = "!@#$%^&*()";
 
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -14,7 +15,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+
 generateBtn.addEventListener("click", writePassword);
 
 //creating the function to generate the password 
@@ -22,22 +23,30 @@ function generatePassword() {
 //developing the string password
 
 var password = "";
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
 
 //length of password 
-const passwordLength = prompt("How long would you like your password?");
-//next line of code helps us check if the password is long enough
+var passwordLength = prompt("How long would you like your password?");
+//series of prompt or confirmed. having a prompts allow user to say yes while confirmed sends a true or false statement
+  var lowerCase = confirm("Do you want a Lowercase?")
+  var upperCase = confirm("Do you want a Upperrcase?")
+  var numbers = confirm("Do you want a Number?")
+  var specialChar = confirm("Do you want a Special Character?")
+//next line helps user know that they need at to say yes to at least one of the conditions
 if ( passwordLength <= 8 || passwordLength >= 128){
   alert("Your password is not long enough. Please try again.");
 }
-//next line helps user know that they need at to say yes to at least one of the conditions
-else if (lowercase === false && upperCase === false && numbers=== false && specialChar === false){
+else if ( upperCase === false && numbers === false && specialChar === false){
   alert("You must choose at least one password criteria")
+  var lowerCase = confirm("Do you want a Lowercase?")
+  var upperCase = confirm("Do you want a Upperrcase?")
+  var numbers = confirm("Do you want a Number?")
+  var specialChar = confirm("Do you want a Special Character?")
+  
 }
 
 
-//series of prompt or confirmed. having a prompts allow user to say yes while confirmed sends a true or false statement
+
 //next couple lines shows the user option for their passwords
 //start of if statements
 
@@ -79,4 +88,5 @@ return password.substring(0,passwordLength);
 function getRandomValue(str){
   const randomIndex = Math.floor(Math.random() * str.length);
   returnstr[randomIndex];
+}
 }
